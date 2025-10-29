d10942f02df0294d153fe75391c62c613f661902
hashir1002 <hashirshakeel25@gmail.com>
2025-10-29 10:44:24 +0500

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import './index.css'
 import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <SpeedInsights/>
   <AppContextProvider>
         <App />
   </AppContextProvider>
